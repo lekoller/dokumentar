abf91b623db6669e6d0f8ce3d402c6abcef03758
Leandro Koller <lekoller@yahoo.com>
2022-12-11 17:23:07 -0300

    feat: Initiate front-end webapp.

---
+++ web/dokumentar-webapp/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { findAll } from "@/libs/api";
+import { useQuery } from "vue-query";
+import HeadFormVue from "../components/HeadForm.vue";
+
+const query = useQuery("findAll", () => findAll());
+</script>
+
+<template>
+    <main>
+        <h1>D O K U M E N T A R</h1>
+        <HeadFormVue />
+    </main>
+</template>
+<TheWelcome />
